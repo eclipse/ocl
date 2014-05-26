9bc3a6f84a80319a9504a5a3d6d5c07661b68cce
Ed Willink <ed@willink.me.uk>
2014-05-26 07:35:27 +0100

    [435732] Use Diagnostic.data[0] to identify marked up resource

--- examples/org.eclipse.ocl.examples.xtext.base.ui/src/org/eclipse/ocl/examples/xtext/base/ui/commands/ValidateCommand.java
+++ examples/org.eclipse.ocl.examples.xtext.base.ui/src/org/eclipse/ocl/examples/xtext/base/ui/commands/ValidateCommand.java
@@ -13,6 +13,8 @@ package org.eclipse.ocl.examples.xtext.base.ui.commands;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.commands.HandlerEvent;
@@ -20,6 +22,10 @@ import org.eclipse.core.commands.IHandlerListener;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.ui.dialogs.DiagnosticDialog;
+import org.eclipse.emf.common.ui.viewer.IViewerProvider;
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.DiagnosticChain;
 import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.emf.ecore.EClass;
@@ -30,17 +36,28 @@ import org.eclipse.emf.ecore.impl.EValidatorRegistryImpl;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.Diagnostician;
+import org.eclipse.emf.ecore.util.EObjectValidator;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.domain.IEditingDomainProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.emf.edit.ui.EMFEditUIPlugin;
 import org.eclipse.emf.edit.ui.action.ValidateAction;
+import org.eclipse.emf.edit.ui.provider.DiagnosticDecorator;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.window.Window;
 import org.eclipse.ocl.examples.pivot.delegate.OCLDelegateDomain;
 import org.eclipse.ocl.examples.pivot.uml.UMLOCLEValidator;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ISetSelectionTarget;
 import org.eclipse.uml2.uml.Profile;
 import org.eclipse.uml2.uml.UMLPackage;
 
@@ -188,11 +205,133 @@ public class ValidateCommand extends ValidateAction
 		if (handlerEvent == null) {
 			throw new NullPointerException();
 		}
-
 		final Object[] listeners = getListeners();
 		for (int i = 0; i < listeners.length; i++) {
 			final IHandlerListener listener = (IHandlerListener) listeners[i];
 			listener.handlerChanged(handlerEvent);
+		}
+	}
+
+	@Override	// FIXME BUG 435735 Overridden to select resource more intelligently for Papyrus
+	protected void handleDiagnostic(Diagnostic diagnostic) {
+		IWorkbenchWindow activeWorkbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		if (activeWorkbenchWindow == null) {
+			return;
+		}
+		int severity = diagnostic.getSeverity();
+		String title = null;
+		String message = null;
+
+		if (severity == Diagnostic.ERROR || severity == Diagnostic.WARNING) {
+			title = EMFEditUIPlugin.INSTANCE.getString("_UI_ValidationProblems_title");
+			message = EMFEditUIPlugin.INSTANCE.getString("_UI_ValidationProblems_message");
+		} else {
+			title = EMFEditUIPlugin.INSTANCE
+				.getString("_UI_ValidationResults_title");
+			message = EMFEditUIPlugin.INSTANCE .getString(severity == Diagnostic.OK
+					? "_UI_ValidationOK_message"
+					: "_UI_ValidationResults_message");
+		}
+
+		int result = 0;
+		if (diagnostic.getSeverity() == Diagnostic.OK) {
+			MessageDialog.openInformation(activeWorkbenchWindow.getShell(), title, message);
+			result = Window.CANCEL;
+		} else {
+			result = DiagnosticDialog.open(activeWorkbenchWindow.getShell(), title, message, diagnostic);
+		}
+
+		ResourceSet resourceSet = domain.getResourceSet();
+		// The following lines replace the simple inherited assumption of resourceSet.getResources().get(0)
+		Resource resource = null;
+		if (eclipseResourcesUtil != null) {
+			List<?> data = diagnostic.getData();
+			if ((data != null) && (data.size() >= 1)) {
+				Object object = data.get(0);
+				if (object instanceof EObject) {
+					resource = ((EObject)object).eResource();
+				}
+			}
+			if (resource == null) {
+				resource = resourceSet.getResources().get(0);
+			}
+		}
+		if (resource != null) {
+			eclipseResourcesUtil.deleteMarkers(resource);
+		}
+
+		if (result == Window.OK) {
+			if (!diagnostic.getChildren().isEmpty()) {
+				List<?> data = (diagnostic.getChildren().get(0)).getData();
+				if (!data.isEmpty() && data.get(0) instanceof EObject) {
+					Object part = activeWorkbenchWindow.getActivePage().getActivePart();
+					if (part instanceof ISetSelectionTarget) {
+						((ISetSelectionTarget) part).selectReveal(new StructuredSelection(data.get(0)));
+					} else if (part instanceof IViewerProvider) {
+						Viewer viewer = ((IViewerProvider) part).getViewer();
+						if (viewer != null) {
+							viewer.setSelection(new StructuredSelection(data.get(0)), true);
+						}
+					}
+				}
+			}
+
+			if (resource != null) {
+				for (Diagnostic childDiagnostic : diagnostic.getChildren()) {
+					eclipseResourcesUtil.createMarkers(resource, childDiagnostic);
+				}
+			}
+		} else {
+			// Trigger direct updating of decorations, if there are adapters.
+			//
+			resource = null;
+		}
+
+		if (resource == null) {
+			// If no markers are produced the decorator won't be able to respond
+			// to marker resource deltas, so inform it directly.
+			//
+
+			// Create a diagnostic for the resource set as a whole.
+			//
+			BasicDiagnostic resourceSetDiagnostic = new BasicDiagnostic(EObjectValidator.DIAGNOSTIC_SOURCE, 0, null, new Object[]{resourceSet});
+
+			// Create a diagnostic for each resource.
+			//
+			Map<Resource, BasicDiagnostic> resourceDiagnostics = new LinkedHashMap<Resource, BasicDiagnostic>();
+			for (Resource r : resourceSet.getResources()) {
+				BasicDiagnostic resourceDiagnostic = new BasicDiagnostic(EObjectValidator.DIAGNOSTIC_SOURCE, 0, null, new Object[]{r});
+				resourceDiagnostics.put(r, resourceDiagnostic);
+			}
+
+			// Just clean up decorations if the dialog was cancelled.
+			//
+			if (result == Dialog.OK) {
+				// Partition the children among the resource diagnostics.
+				//
+				for (Diagnostic child : diagnostic.getChildren()) {
+					List<?> data = child.getData();
+					if (!data.isEmpty()) {
+						Object object = data.get(0);
+						if (object instanceof EObject) {
+							BasicDiagnostic resourceDiagnostic = resourceDiagnostics.get(((EObject) object).eResource());
+							if (resourceDiagnostic != null) {
+								resourceDiagnostic.add(child);
+							}
+						}
+					}
+				}
+			}
+
+			// Add the resource diagnostics to the resource set diagnostic.
+			//
+			for (Diagnostic resourceDiagnostic : resourceDiagnostics.values()) {
+				resourceSetDiagnostic.add(resourceDiagnostic);
+			}
+
+			// Inform any decorators.
+			//
+			DiagnosticDecorator.DiagnosticAdapter.update(resourceSet, resourceSetDiagnostic);
 		}
 	}
 
