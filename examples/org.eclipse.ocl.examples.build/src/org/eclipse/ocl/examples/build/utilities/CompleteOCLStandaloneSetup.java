542476c0b9ac433a94765e74fc75ea4e56a8c8fc
Ed Willink <ed@willink.me.uk>
2014-12-20 13:03:46 +0000

    [ocl25] Fix bit rot in conversion scripts

---
+++ examples/org.eclipse.ocl.examples.build/src/org/eclipse/ocl/examples/build/utilities/CompleteOCLStandaloneSetup.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2014 E.D.Willink and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     E.D.Willink - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ocl.examples.build.utilities;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Initializes OCL support.
+ */
+public class CompleteOCLStandaloneSetup
+{
+	private Logger log = Logger.getLogger(getClass());
+
+	public CompleteOCLStandaloneSetup() {
+		log.info("Registering CompleteOCLStandaloneSetup");
+		org.eclipse.ocl.xtext.completeocl.CompleteOCLStandaloneSetup.doSetup();
+	}
+}
